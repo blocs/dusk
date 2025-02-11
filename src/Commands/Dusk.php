330807617a5caa47b401a71eb9bc6f56011e923c
Hiroyuki Yada <yada@linear-jp.biz>
2025-02-11 19:47:30 +0900

    add commandを追加
    Commands名を変更

--- src/Commands/Dusk.php
+++ src/Commands/Dusk.php
@@ -2,101 +2,257 @@
 
 namespace Blocs\Commands;
 
+use Facebook\WebDriver\Exception\InvalidSessionIdException;
+use Facebook\WebDriver\Exception\NoSuchWindowException;
 use Illuminate\Console\Command;
-use OpenAI\Laravel\Facades\OpenAI;
 
 class Dusk extends Command
 {
-    protected $signature = 'blocs:dusk {trait} {manual}';
-    protected $description = 'Develop laravel dusk browser tests';
+    use DuskOpenAiTrait;
+    use DuskTestTrait;
+
+    protected $signature = 'blocs:dusk {script}';
+    protected $description = 'Support laravel dusk browser tests';
+    private $browser;
+    private $indent;
 
     public function handle()
     {
-        $trait = $this->argument('trait');
-        if (!file_exists($trait)) {
+        $script = $this->argument('script');
+        file_exists($script) || $script = base_path($script);
+        if (!file_exists($script)) {
+            $this->error('Script not found');
+
             return;
         }
 
-        $manual = $this->argument('manual');
-        if (!file_exists($manual)) {
-            return;
-        }
+        do {
+            // Retrieve all functions from $script
+            $originalScript = file_get_contents($script);
 
-        $this->updateTrait($trait, $manual);
+            $functions = [];
+            preg_match_all('/\s*function\s*(.*?)\(/', $originalScript, $functions);
+
+            // Choose action
+            $actions = [];
+            foreach ($functions[1] as $function) {
+                // Add test functions
+                if (0 === strpos($function, 'test')) {
+                    $actions[] = $function.' update';
+                    $actions[] = $function.' run';
+                }
+            }
+            $actions[] = 'exit';
+            $action = $this->anticipate('Action', $actions);
+
+            // run
+            if (' run' === substr($action, -4)) {
+                $function = substr($action, 0, -4);
+                \Artisan::call('dusk --browse '.$script.' --filter '.$function);
+                continue;
+            }
+
+            // exit
+            if ('exit' === strtolower($action) || 'bye' === strtolower($action)) {
+                isset($this->browser) && $this->browser->quit();
+                exit;
+            }
+
+            // update
+            if (' update' !== substr($action, -7)) {
+                continue;
+            }
+
+            // Target function
+            $function = substr($action, 0, -7);
+
+            $originalScript = file_get_contents($script);
+            list($beforeFunction, $functionContent, $afterFunction) = $this->splitScript($originalScript, $function);
+
+            // Retrieve comments
+            $funstionContents = explode("\n", $functionContent);
+            strlen(last($funstionContents)) || array_pop($funstionContents);
+
+            $beforeComment = '';
+            while ($funstionContents) {
+                if (preg_match('/^\s*\/\/\s*/', $funstionContents[0])) {
+                    break;
+                }
+
+                $beforeComment .= array_shift($funstionContents)."\n";
+            }
+
+            $afterComment = '';
+            while ($funstionContents) {
+                if (preg_match('/[^\s\}\)\;]/', last($funstionContents))) {
+                    break;
+                }
+
+                $afterComment = array_pop($funstionContents)."\n".$afterComment;
+            }
+
+            $comments = [];
+            foreach ($funstionContents as $funstionContent) {
+                if (preg_match('/^\s*\/\/\s*/', $funstionContent)) {
+                    $comments[] = [
+                        'comment' => $funstionContent,
+                        'script' => '',
+                    ];
+                    continue;
+                }
+
+                $comments[count($comments) - 1]['script'] .= $funstionContent."\n";
+            }
+
+            // Open browser
+            $this->openBrowser();
+
+            $updateFlag = true;
+            foreach ($comments as $num => $comment) {
+                $scriptContent = $comment['script'];
+                while (1) {
+                    list($this->indent, $operation) = explode('//', $comment['comment'], 2);
+                    $operation = trim($operation);
+
+                    $this->line(trim($comment['comment']));
+
+                    if (empty(trim($scriptContent))) {
+                        // generate
+                        $scriptContent = $this->generateCode($operation);
+
+                        if (false === strpos($scriptContent, '$browser')) {
+                            $comments[$num]['script'] = $this->addIndent($scriptContent, '// ')."\n";
+                            $comment['script'] = $comments[$num]['script'];
+                        } else {
+                            $comments[$num]['script'] = $this->addIndent($scriptContent)."\n";
+                            $comment['script'] = $comments[$num]['script'];
+                        }
+                    }
+
+                    if (false === strpos($scriptContent, '$browser')) {
+                        // Additional question
+                        $action = $this->anticipate(trim($scriptContent)."\n", [$operation, 'skip', 'stop']);
+                    } else {
+                        $action = $this->anticipate(trim($scriptContent)."\n", ['execute', $operation, 'update', 'skip', 'stop'], 'execute');
+                    }
+
+                    // execute
+                    if ('execute' === strtolower($action)) {
+                        if ($this->executeScript($scriptContent)) {
+                            break;
+                        }
+                        continue;
+                    }
+
+                    // update
+                    if ('update' === strtolower($action)) {
+                        break 2;
+                    }
+
+                    // skip
+                    if ('skip' === strtolower($action)) {
+                        break;
+                    }
+
+                    // stop
+                    if ('stop' === strtolower($action)) {
+                        $updateFlag = false;
+                        break 2;
+                    }
+
+                    // generate
+                    $comments[$num]['comment'] = $this->addIndent($action, '// ');
+                    $comment['comment'] = $comments[$num]['comment'];
+
+                    $scriptContent = $this->generateCode($action);
+
+                    if (false === strpos($scriptContent, '$browser')) {
+                        $comments[$num]['script'] = $this->addIndent($scriptContent, '// ')."\n";
+                        $comment['script'] = $comments[$num]['script'];
+                    } else {
+                        $comments[$num]['script'] = $this->addIndent($scriptContent)."\n";
+                        $comment['script'] = $comments[$num]['script'];
+                    }
+                }
+            }
+
+            if ($updateFlag) {
+                $this->line('Update script');
+
+                $updatedScript = '';
+                foreach ($comments as $comment) {
+                    empty($comment['comment']) || $updatedScript .= $comment['comment']."\n";
+                    empty($comment['script']) || $updatedScript .= $comment['script'];
+                }
+
+                $originalScript = $beforeFunction.$beforeComment.$updatedScript.$afterComment.$afterFunction;
+                file_put_contents($script, $originalScript);
+            }
+
+            isset($this->browser) && $this->browser->quit();
+        } while (1);
     }
 
-    private function updateTrait($trait, $manual)
+    private function addIndent($scriptContent, $preset = '')
     {
-        $messageContent = [
-            [
-                'type' => 'text',
-                'text' => file_get_contents(base_path('tests/Browser/role/user.md'))."\n",
-            ],
-            [
-                'type' => 'text',
-                'text' => "# 操作メソッド\n".file_get_contents($manual.'/method.md')."\n",
-            ],
-            [
-                'type' => 'text',
-                'text' => "# 入力トレイト\n".file_get_contents($trait)."\n",
-            ],
-            [
-                'type' => 'text',
-                'text' => file_get_contents(base_path('tests/Browser/role/sample.md'))."\n",
-            ],
-        ];
-
-        if (file_exists($manual.'/source.html')) {
-            $messageContent[] = [
-                'type' => 'text',
-                'text' => "# 画面のHTML\n```html\n".file_get_contents($manual.'/source.html')."\n```\n",
-            ];
+        $result = '';
+        foreach (explode("\n", $scriptContent) as $line) {
+            $result .= $this->indent.$preset.$line."\n";
         }
 
-        if (file_exists($manual.'/screenshot.png')) {
-            $messageContent[] = [
-                'type' => 'image_url',
-                'image_url' => [
-                    'url' => 'data:image/png; base64, '.base64_encode(file_get_contents($manual.'/screenshot.png')),
-                ],
-            ];
+        return $result;
+    }
+
+    private function executeScript($scriptContent)
+    {
+        $browser = $this->browser;
+
+        try {
+            eval($scriptContent);
+        } catch (NoSuchWindowException $e) {
+            $browser->quit();
+            $this->error('Browser closed');
+            exit;
+        } catch (InvalidSessionIdException $e) {
+            $browser->quit();
+            $this->error('Browser closed');
+            exit;
+        } catch (\Throwable $e) {
+            $this->error($e->getMessage());
+
+            return false;
         }
 
-        $result = OpenAI::chat()->create([
-            'model' => 'gpt-4o',
-            'messages' => [
-                [
-                    'role' => 'system',
-                    'content' => [
-                        [
-                            'type' => 'text',
-                            'text' => file_get_contents(base_path('tests/Browser/role/system.md')),
-                        ],
-                    ],
-                ],
-                [
-                    'role' => 'user',
-                    'content' => $messageContent,
-                ],
-                [
-                    'role' => 'assistant',
-                    'content' => [
-                        [
-                            'type' => 'text',
-                            'text' => file_get_contents(base_path('tests/Browser/role/assistant.md')),
-                        ],
-                    ],
-                ],
-            ],
-        ]);
+        return true;
+    }
 
-        $traitContent = $result->choices[0]->message->content;
-        $traitContent = str_replace('```php', '', $traitContent);
-        $traitContent = str_replace('```', '', $traitContent);
-        $traitContent = trim($traitContent);
+    private function splitScript($scriptContent, $function)
+    {
+        $scriptContent = explode("\n", rtrim($scriptContent));
+        $beforeFunction = '';
+        $functionContent = '';
+        $afterFunction = '';
 
-        if (false !== strpos($traitContent, '<?php')) {
-            file_put_contents($trait, $traitContent);
+        $flag = false;
+        foreach ($scriptContent as $num => $line) {
+            if ($flag && (preg_match('/\s+function\s+/', $line) || count($scriptContent) <= $num + 1)) {
+                $flag = false;
+            }
+            if (preg_match('/\s+function\s+'.$function.'\(/', $line)) {
+                $flag = true;
+            }
+
+            if (!$flag && empty($functionContent)) {
+                $beforeFunction .= $line."\n";
+            }
+            if ($flag) {
+                $functionContent .= $line."\n";
+            }
+            if (!$flag && !empty($functionContent)) {
+                $afterFunction .= $line."\n";
+            }
         }
+
+        return [$beforeFunction, $functionContent, $afterFunction];
     }
 }
---
+++ src/Commands/DuskTestTrait.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Blocs\Commands;
+
+use Facebook\WebDriver\Chrome\ChromeOptions;
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Illuminate\Support\Collection;
+use Laravel\Dusk\Chrome\SupportsChrome;
+use Laravel\Dusk\Concerns\ProvidesBrowser;
+use PHPUnit\Framework\Attributes\BeforeClass;
+
+trait DuskTestTrait
+{
+    use ProvidesBrowser;
+    use SupportsChrome;
+
+    /**
+     * Prepare for Dusk test execution.
+     */
+    #[BeforeClass]
+    public static function prepare(): void
+    {
+        if (!static::runningInSail()) {
+            static::startChromeDriver(['--port=9515']);
+        }
+    }
+
+    /**
+     * Create the RemoteWebDriver instance.
+     */
+    protected function driver(): RemoteWebDriver
+    {
+        $options = (new ChromeOptions())->addArguments(collect([
+            $this->shouldStartMaximized() ? '--start-maximized' : '--window-size=1920,1080',
+            '--disable-search-engine-choice-screen',
+        ])->unless($this->hasHeadlessDisabled(), function (Collection $items) {
+            return $items->merge([
+                '--disable-gpu',
+                '--headless=new',
+            ]);
+        })->all());
+
+        return RemoteWebDriver::create(
+            $_ENV['DUSK_DRIVER_URL'] ?? env('DUSK_DRIVER_URL') ?? 'http://localhost:9515',
+            DesiredCapabilities::chrome()->setCapability(
+                ChromeOptions::CAPABILITY, $options
+            )
+        );
+    }
+
+    /**
+     * Determine whether the Dusk command has disabled headless mode.
+     */
+    protected function hasHeadlessDisabled(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine if the browser window should start maximized.
+     */
+    protected function shouldStartMaximized(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine if the tests are running within Laravel Sail.
+     *
+     * @return bool
+     */
+    protected static function runningInSail()
+    {
+        return isset($_ENV['LARAVEL_SAIL']) && '1' == $_ENV['LARAVEL_SAIL'];
+    }
+
+    private function openBrowser()
+    {
+        $this->prepare();
+
+        $this->browser = $this->newBrowser($this->createWebDriver());
+        $this->browser::$storeScreenshotsAt = base_path('tests/Browser/screenshots');
+        $this->browser::$storeSourceAt = base_path('tests/Browser/source');
+    }
+}
